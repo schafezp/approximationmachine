7dc47ca5bdc8b5eea92ec197981fd6842cc3be29
unknown <dituccjv@DITUCCJV-W530G2.rose-hulman.edu>
2016-01-29 20:18:55 -0500

    Corrected Misspelling in Authors Line

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 %This will be the center of execution for the matlab scripts that we write.
 %
-%Authors: Zachary Schafer, James DiTucci, Maxim Zaman, Stephanie Lee
+%Authors: Zachary Fuckface, James DiTucci, Maxim Zaman, Stephanie Lee
 %Class: MA373-01 winter quarter 2015-2016
 %Professor: Dr. Graves
 
