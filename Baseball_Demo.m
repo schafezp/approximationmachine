c57c2e3bb3d723c12be82f146bf2cf80b31afd1c
unknown <dituccjv@DITUCCJV-W530G2.rose-hulman.edu>
2016-02-11 23:46:45 -0500

    Created a demo script to show an instance when TLS excels over OLS

--- Baseball_Demo.m
+++ Baseball_Demo.m
@@ -1,3 +1,4 @@
+close all
 data = csvread('teams_post1999.csv',1,3);
 %Grab columns of data to use on our function
 wins = data(:,1);
@@ -7,5 +8,4 @@ Bat_Ave = data(:,5);
 HR = data(:,8);
 ERA = data(:,17);
 %Call the main
-main(HR,wins,'')
-%main(x,y,'')
+main(hits,runs)
