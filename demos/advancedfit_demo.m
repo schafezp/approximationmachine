3034c78a7ceab12c5527297dbb8377a80e87dbbf
Zachary Schafer <schafezp@rose-hulman.edu>
2016-02-15 15:00:40 -0500

    Example of when splines are great!

---
+++ demos/advancedfit_demo.m
@@ -0,0 +1,17 @@
+currentpath = cd('..');
+parentpath = pwd()
+
+x = linspace(-2,2,100);
+%y = tanh(x/2) + exp(x.^1.3/4) + sin(3*x);
+y = 1./(1+5*x.^2) + tanh(x/2);
+%plot(x,y,'r')
+subplot(2,1,1)
+main(x,y,'spline,poly9,poly12')
+title('Difficult Function: y = 1/(1+5*x^2) + tanh(x/2)')
+subplot(2,1,2)
+main(x,y,'linear,poly2,poly4,poly6,poly8,spline')
+title('Difficult Function: y = 1/(1+5*x^2) + tanh(x/2)')
+%possible
+
+%restore previous path
+cd(currentpath);
