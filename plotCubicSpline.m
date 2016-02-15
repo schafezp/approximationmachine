e5f283c5e8f3102e3b6571817bd4cdd404fc1ef8
Zachary Schafer <schafezp@rose-hulman.edu>
2016-02-15 14:35:55 -0500

    plotCubicSpline now plots inner points

--- plotCubicSpline.m
+++ plotCubicSpline.m
@@ -1,8 +1,9 @@
 function [] = plotCubicSpline(x,y,jump,color)
 subx = x(1:jump:length(x));
 suby = y(1:jump:length(y));
-intx = linspace(min(subx),max(subx),(max(subx)-min(subx)));
+inner_points = 10;
+intx = linspace(min(subx),max(subx),inner_points*(max(subx)-min(subx)));
 cubicy3 = cubicSpline(subx,suby,intx);
-plot(intx,cubicy3,color)
+plot(intx,cubicy3,color);
 % $$$ legendStr = sprintf('jump distance: %d',jump)
 % $$$ legappend(legendStr)
