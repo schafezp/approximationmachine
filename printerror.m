146372369729ffe3b5acb386932360b715310689
Zachary Schafer <schafezp@rose-hulman.edu>
2016-02-16 09:05:31 -0500

    Removed longer print statement in printerror. We only need to return 5 precision points.

--- printerror.m
+++ printerror.m
@@ -1,6 +1,6 @@
 function [] =  printerror(r2,rmse,dispstring)
         fprintf('----------------------\n')
         fprintf([dispstring '\n'] )
-        fprintf('R squared Coefficient: %.9f \n',r2);
+        fprintf('R squared Coefficient: %f \n',r2);
         fprintf('Root mean square: %f \n',rmse);
 end
