efb8608a587540cce090793cc949c237d3c2bb7c
Zachary Schafer <schafezp@rose-hulman.edu>
2016-02-16 09:08:46 -0500

    Modify subplot orientation in advancedfitdemo

--- demos/advancedfit_demo.m
+++ demos/advancedfit_demo.m
@@ -15,11 +15,12 @@ parentpath = pwd()
 
 x = linspace(-2,2,100);
 y = 1./(1+5*x.^2) ;
+
 subplot(2,1,1)
-datfit(x,y,'spline,poly7,poly10')
+datfit(x,y,'spline,poly2,poly4,poly6')
 title('Difficult Function: y = 1/(1+5*x^2)')
 subplot(2,1,2)
-datfit(x,y,'spline,poly2,poly4,poly6')
+datfit(x,y,'spline,poly7,poly10')
 title('Difficult Function: y = 1/(1+5*x^2)')
 
 % $$$ x = linspace(-2,2,100);
